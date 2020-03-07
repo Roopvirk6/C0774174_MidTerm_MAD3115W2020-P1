0ca171c445928985585b740758ddbd44d8bdc5f9
ikroop <virkikroop13@gmail.com>
2020-03-06 20:35:16 -0500

    [ikroop] data storage class created

--- C0774174_MidTerm_MAD3115W2020-P1/DataStorage.swift
+++ C0774174_MidTerm_MAD3115W2020-P1/DataStorage.swift
@@ -9,5 +9,29 @@
 import Foundation
 class DataStorage
 {
-    
+    private static let instance = DataStorage()
+    private lazy var customerArray: [Customer] = []
+    private init(){}
+     
+    static func getInstance() -> DataStorage{
+      return instance
+    }
+     
+    func addCustomer(customer: Customer)
+    {
+      self.customerArray.append(customer)
+    }
+     
+    func getAllCustomers() -> [Customer]
+    {
+      return self.customerArray
+    }
+     
+    func loadData()
+    {
+      customerArray.append(Customer(customerId: 1, firstName: "roop", lastName: "virk", emailId: "roopvirk@gmail.com"))
+        customerArray.append(Customer(customerId: 1, firstName: "aman", lastName: "kaur", emailId: "aman@gmail.com"))
+        
+          
+    }
 }
