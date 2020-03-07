99af70d765e86ff0844096d2f0531c2badd51b10
ikroop <virkikroop13@gmail.com>
2020-03-06 19:45:17 -0500

    [ikroop] data storage class created

---
+++ C0774174_MidTerm_MAD3115W2020-P1/DataStorage.swift
@@ -0,0 +1,13 @@
+//
+//  DataStorage.swift
+//  C0774174_MidTerm_MAD3115W2020-P1
+//
+//  Created by MacStudent on 2020-03-06.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+class DataStorage
+{
+    
+}
