0705a4d08b6478bcebe9027d1590c7f5c7321608
ikroop <virkikroop13@gmail.com>
2020-03-11 18:26:07 -0400

    [ikroop] internet class created

---
+++ C0774174_MidTerm_MAD3115W2020-P1/Internet.swift
@@ -0,0 +1,9 @@
+//
+//  Internet.swift
+//  C0774174_MidTerm_MAD3115W2020-P1
+//
+//  Created by MacStudent on 2020-03-11.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
