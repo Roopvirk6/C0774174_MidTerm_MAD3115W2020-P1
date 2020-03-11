c14e2a478fca6120ce8063c31a4a68d6a6169668
ikroop <virkikroop13@gmail.com>
2020-03-11 18:14:02 -0400

    [ikroop] class hydro created and variables added

---
+++ C0774174_MidTerm_MAD3115W2020-P1/Hydro.swift
@@ -0,0 +1,38 @@
+//
+//  Hydro.swift
+//  C0774174_MidTerm_MAD3115W2020-P1
+//
+//  Created by MacStudent on 2020-03-11.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+class HydroBill: Bill{
+    
+    
+    var agencyName : String
+    var unitConsumed : Int
+    var billAmount : Float
+    
+    
+    init(billID : Int , billDate : String, billType : biTypes, agencyName : String, unitConsumed : Int , billAmount : Float) {
+        self.agencyName = agencyName
+        self.unitConsumed = unitConsumed
+        self.billAmount = billAmount
+        super.init(billId : billID, billDate : billDate, billType : billType)
+        
+    }
+    
+  
+    override func display() {
+        print("*****************************")
+        print("       Bill Details      ")
+        print("*****************************")
+        print("Bill ID : \(billId)")
+        print("BillDate : \(billDate)")
+        print("Bill Type : \(billType)")
+        print("Agency Name : \(agencyName)")
+        print("Unit Consumed : \(unitConsumed)")
+        print("Bill Amount : $\(billAmount)")
+    }
+}
