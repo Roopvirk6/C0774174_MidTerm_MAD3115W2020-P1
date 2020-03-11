150bd3072a4c17cbdc74546b236e8cad5e9d9c92
ikroop <virkikroop13@gmail.com>
2020-03-11 19:11:23 -0400

    [ikroop] extension for float is created

---
+++ C0774174_MidTerm_MAD3115W2020-P1/Extensions/floatExtension.swift
@@ -0,0 +1,16 @@
+//
+//  floatExtension.swift
+//  C0774174_MidTerm_MAD3115W2020-P1
+//
+//  Created by MacStudent on 2020-03-11.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+extension Float
+{
+    func fltToString() -> String
+    {
+        return String(self)
+    }
+}
